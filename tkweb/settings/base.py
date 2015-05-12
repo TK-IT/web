6736bb0ebb2e68895a5dc39be0ec7e924ab09958
Mathias Dannesbo <neic@neic.dk>
2015-05-13 01:33:30 +0200

    Comment settings

--- tkweb/settings/base.py
+++ tkweb/settings/base.py
@@ -1,11 +1,5 @@
 """
 Django settings for tkweb project.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/1.7/topics/settings/
-
-For the full list of settings and their values, see
-https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -13,57 +7,65 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 # Application definition
-
-INSTALLED_APPS = [
+DEFAULT_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'tkweb.apps.gallery',
+]
+
+THIRD_PARTY_APPS = [
     'imagekit',
     'jfu',
 ]
 
-MIDDLEWARE_CLASSES = (
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.auth.middleware.SessionAuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
-)
+LOCAL_APPS = [
+    'tkweb.apps.gallery',
+]
 
-ROOT_URLCONF = 'tkweb.urls'
-
-WSGI_APPLICATION = 'tkweb.wsgi.application'
+INSTALLED_APPS = DEFAULT_APPS + THIRD_PARTY_APPS + LOCAL_APPS
 
 
 # Internationalization
-# https://docs.djangoproject.com/en/1.7/topics/i18n/
+# --------------------
+# https://docs.djangoproject.com/en/1.8/topics/i18n/
 
-LANGUAGE_CODE = 'da-dk'
-
-TIME_ZONE = 'UTC'
-
+TIME_ZONE = 'Europe/Copenhagen'
+LANGUAGE_CODE = 'en-us'
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
 
+# URLs
+# ----
 
-# Absolute filesystem path to the directory that will hold user-uploaded files.
-# Example: "/var/www/example.com/media/"
-MEDIA_ROOT = 'media'
+ROOT_URLCONF = 'tkweb.urls'
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
 MEDIA_URL = '/media/'
+
+
+# URL prefix for static files.
+# Example: "http://example.com/static/", "http://static.example.com/"
+STATIC_URL = '/static/'
+
+LOGIN_URL = '/admin/login'
+
+# Media files
+# -----------
+
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/var/www/example.com/media/"
+MEDIA_ROOT = 'media'
+
+
+# Static files
+# ------------
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -71,17 +73,12 @@ MEDIA_URL = '/media/'
 # Example: "/var/www/example.com/static/"
 STATIC_ROOT = 'staticr'
 
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.7/howto/static-files/
-STATIC_URL = '/static/'
-
 
 # Additional locations of static files
 STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
-    # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
-    "static",
+    "static-src",
 )
 
 # List of finder classes that know how to find static files in
@@ -89,15 +86,10 @@ STATICFILES_DIRS = (
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
-
-TEMPLATE_LOADERS = (
-    'django.template.loaders.filesystem.Loader',
-    'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
-)
+# Templates
+# ---------
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
@@ -108,6 +100,23 @@ TEMPLATE_DIRS = (
 
 TEMPLATE_CONTEXT_PROCESSORS = (
     'django.contrib.auth.context_processors.auth',
-    'django.core.context_processors.request',
     'django.core.context_processors.static',
+    'django.core.context_processors.request',
 )
+
+
+# Middleware
+# ----------
+
+MIDDLEWARE_CLASSES = (
+    'django.middleware.common.CommonMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+)
+
+# Test
+# ----
+
+TEST_RUNNER = 'django.test.runner.DiscoverRunner'
