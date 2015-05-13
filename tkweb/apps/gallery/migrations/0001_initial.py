a11af1173ab45b4ab48c347a37381ec0f4599e28
Mathias Dannesbo <neic@neic.dk>
2015-05-13 02:33:07 +0200

    Add initial migration to gallery app

---
+++ tkweb/apps/gallery/migrations/0001_initial.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Album',
+            fields=[
+                ('id', models.AutoField(serialize=False, auto_created=True, primary_key=True, verbose_name='ID')),
+                ('title', models.CharField(max_length=200)),
+                ('gfyear', models.PositiveSmallIntegerField()),
+                ('description', models.TextField(blank=True)),
+            ],
+        ),
+    ]
