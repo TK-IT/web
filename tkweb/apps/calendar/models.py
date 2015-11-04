be1a1db5a12e7bcc4644cfaa9e461966b8e0220b
Mathias Rav <rav@cs.au.dk>
2015-11-04 18:11:00 +0100

    Use search instead of split

--- tkweb/apps/calendar/models.py
+++ tkweb/apps/calendar/models.py
@@ -14,9 +14,9 @@ class Event(models.Model):
         """Extract facebook url from description"""
 
         p = re.compile('((https?://)?(www\.)?(facebook|fb)\.com[a-z0-9/]*)')
-        desc_l = p.split(self.description)
-        if len(desc_l)>=2:
-            self.facebook = desc_l[1]
-            self.description = desc_l[0].strip()
+        mo = p.search(self.description)
+        if mo is not None:
+            self.facebook = mo.group(0)
+            self.description = self.description[:mo.begin(0)]
 
         super(Event, self).save()
