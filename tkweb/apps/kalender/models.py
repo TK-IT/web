73361c75e94d06a47e5004e8cb320ac56241cc6c
Mathias Dannesbo <neic@neic.dk>
2015-11-04 16:47:02 +0100

    Add facebook links to kalender
    Closes #3

--- tkweb/apps/kalender/models.py
+++ tkweb/apps/kalender/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+import re
 
 class Event(models.Model):
     class Meta:
@@ -8,3 +9,14 @@ class Event(models.Model):
     date = models.DateField()
     description = models.TextField()
     facebook = models.CharField(max_length=200)
+
+    def save(self):
+        """Extract facebook url from description"""
+
+        p = re.compile('((https?://)?(www\.)?(facebook|fb)\.com[a-z0-9/]*)')
+        desc_l = p.split(self.description)
+        if len(desc_l)>=2:
+            self.facebook = desc_l[1]
+            self.description = desc_l[0].strip()
+
+        super(Event, self).save()
