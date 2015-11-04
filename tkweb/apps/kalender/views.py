d306ec0aaf17fd81564d2ac39782632f1b6402be
Mathias Dannesbo <neic@neic.dk>
2015-11-04 16:47:43 +0100

    Fix double appearance of today's event

--- tkweb/apps/kalender/views.py
+++ tkweb/apps/kalender/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from tkweb.apps.kalender.models import Event
 from datetime import date, timedelta
 def kalender(request):
-    futureEvents = Event.objects.filter(date__gt=(date.today() - timedelta(days=1)))
-    pastEvents = Event.objects.filter(date__range=((date.today() - timedelta(days=365)), date.today()))
+    todayish = date.today() - timedelta(days=1)
+    yearAgo = date.today() - timedelta(days=365)
+
+    futureEvents = Event.objects.filter(date__gt=(todayish))
+    pastEvents = Event.objects.filter(date__range=(yearAgo, todayish))
 
     context = {'futureEvents' : futureEvents,
                'pastEvents' : pastEvents}
