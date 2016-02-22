ebb384a0d0fef8a149a1585d9c65b37e3d6295d0
Mathias Rav <FORM13@TAAGEKAMMERET.dk>
2016-02-22 18:25:56 +0100

    Turn urlpatterns into just a list

--- tkweb/apps/gallery/urls.py
+++ tkweb/apps/gallery/urls.py
@@ -1,34 +1,34 @@
 from django.conf.urls import patterns, url
 from tkweb.apps.gallery import views
 
-urlpatterns = patterns('',
-                       # Gallery overview
-                       url(r'^$',
-                           'tkweb.apps.gallery.views.gallery',
-                           name='gallery_index'),
-                       url(r'^(?P<gfyear>\d+)$',
-                           'tkweb.apps.gallery.views.gallery',
-                           name='gfyear'),
+urlpatterns = [
+    # Gallery overview
+    url(r'^$',
+        'tkweb.apps.gallery.views.gallery',
+        name='gallery_index'),
+    url(r'^(?P<gfyear>\d+)$',
+        'tkweb.apps.gallery.views.gallery',
+        name='gfyear'),
 
-                       # Album overview
-                       url(r'^(?P<gfyear>\d+)/(?P<album_slug>[^/]+)$',
-                           'tkweb.apps.gallery.views.album',
-                           name='album'),
-                       url(r'^(?P<album_slug>[^/]+)$',
-                           'tkweb.apps.gallery.views.album',
-                           {'gfyear': '0000'}),
+    # Album overview
+    url(r'^(?P<gfyear>\d+)/(?P<album_slug>[^/]+)$',
+        'tkweb.apps.gallery.views.album',
+        name='album'),
+    url(r'^(?P<album_slug>[^/]+)$',
+        'tkweb.apps.gallery.views.album',
+        {'gfyear': '0000'}),
 
-                       # Single images
-                       url((r'^(?P<gfyear>\d+)/(?P<album_slug>[^/]+)/'
-                            r'(?P<image_slug>[^/]+)$'),
-                           'tkweb.apps.gallery.views.image',
-                           name='image'),
-                       url(r'^(?P<album_slug>[^/]+)/(?P<image_slug>[^/]+)$',
-                           'tkweb.apps.gallery.views.image',
-                           name='image'),
+    # Single images
+    url((r'^(?P<gfyear>\d+)/(?P<album_slug>[^/]+)/'
+         r'(?P<image_slug>[^/]+)$'),
+        'tkweb.apps.gallery.views.image',
+        name='image'),
+    url(r'^(?P<album_slug>[^/]+)/(?P<image_slug>[^/]+)$',
+        'tkweb.apps.gallery.views.image',
+        name='image'),
 
-                       # JFU upload
-                       url(r'^upload/',
-                           'tkweb.apps.gallery.views.upload',
-                           name='jfu_upload'),
-                      )
+    # JFU upload
+    url(r'^upload/',
+        'tkweb.apps.gallery.views.upload',
+        name='jfu_upload'),
+]
