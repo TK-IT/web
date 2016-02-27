34e8e65d4b17bd93d696f41a94d2747e18256623
Mathias Rav <FORM13@TAAGEKAMMERET.dk>
2016-02-27 15:18:07 +0100

    Enable admin inlines

--- idm/admin.py
+++ idm/admin.py
@@ -2,11 +2,21 @@ from django.contrib import admin
 
 from idm.models import (
     Profile, Best,
+    Title, GradGroupMembership,
 )
+
+
+class ProfileTitleAdmin(admin.TabularInline):
+    model = Title
+
+
+class ProfileGradGroupAdmin(admin.TabularInline):
+    model = GradGroupMembership
 
 
 class ProfileAdmin(admin.ModelAdmin):
     list_display = ('navn', 'email', 'accepteremail', 'accepterdirektemail')
+    inlines = [ProfileTitleAdmin, ProfileGradGroupAdmin]
 
 
 class BestAdmin(admin.ModelAdmin):
