c95624a1b1a293ab511ec34ea2bad3d996e023da
Mathias Dannesbo <neic@neic.dk>
2016-03-21 12:33:20 -0500

    Fix encoding, whitespace, indetation, line lenght

--- tkweb/apps/gallery/static/tkgal.js
+++ tkweb/apps/gallery/static/tkgal.js
@@ -1,89 +1,88 @@
 permlinkBase = "";
 galleryContainer = ".tkgal_container";
 
-// skjuler alt undtagen det ønskede element med attributen permlink="permlink"
+// skjuler alt undtagen det Ã¸nskede element med attributen permlink="permlink"
 function changeCurrent(permlink) {
-  loadContent(permlink);
-  $elements=$(galleryContainer+" > *")
-  $elements.removeClass("current"); // skjul nuværende
-  numberOfElements = $elements.length;
+    loadContent(permlink);
+    $elements=$(galleryContainer+" > *");
+    $elements.removeClass("current"); // skjul nuvÃ¦rende
+    numberOfElements = $elements.length;
 
-  $current =$("[permlink="+permlink+"]");
-  $current.addClass("current"); // vis ønskede
+    $current =$("[permlink="+permlink+"]");
+    $current.addClass("current"); // vis Ã¸nskede
 
-  window.history.replaceState(null,null, permlink); // tilpas viste url i browseren (skal tilpasses django url til samme indhold)
+    // tilpas viste url i browseren (skal tilpasses django url til samme
+    // indhold)
+    window.history.replaceState(null,null, permlink);
 
-  prevIndex = ($current.index() - 1)%numberOfElements;
-  nextIndex = ($current.index() + 1)%numberOfElements;
-  prevPermlink=$(galleryContainer+" > *").eq(prevIndex).attr("permlink");
-  nextPermlink=$(galleryContainer+" > *").eq(nextIndex).attr("permlink");
-  
-  $(".tkgal_link.prev").attr("link", prevPermlink);
-  $(".tkgal_link.next").attr("link", nextPermlink);
-  
-  url_base = $(".tkgal_url_base").attr("href");
-  $(".tkgal_link.prev").attr("href", url_base + "/" + prevPermlink);
-  $(".tkgal_link.next").attr("href", url_base + "/" + nextPermlink);
-  
-  
-  loadContent(prevPermlink);
-  loadContent(nextPermlink);
+    prevIndex = ($current.index() - 1)%numberOfElements;
+    nextIndex = ($current.index() + 1)%numberOfElements;
+    prevPermlink=$(galleryContainer+" > *").eq(prevIndex).attr("permlink");
+    nextPermlink=$(galleryContainer+" > *").eq(nextIndex).attr("permlink");
+
+    $(".tkgal_link.prev").attr("link", prevPermlink);
+    $(".tkgal_link.next").attr("link", nextPermlink);
+
+    url_base = $(".tkgal_url_base").attr("href");
+    $(".tkgal_link.prev").attr("href", url_base + "/" + prevPermlink);
+    $(".tkgal_link.next").attr("href", url_base + "/" + nextPermlink);
+
+    loadContent(prevPermlink);
+    loadContent(nextPermlink);
 }
 
 // load indhold, ved at fjerne dummy-attributer
 function loadContent(permlink) {
-	$("[permlink="+permlink+"] [data-src]").each(function( index ) {
-		$(this).replaceWith($(this)[0].outerHTML.replace("data-src=","src="));
-	});
-	$("[permlink="+permlink+"] [data-srcset]").each(function( index ) {
-		$(this).replaceWith($(this)[0].outerHTML.replace("data-srcset=","srcset="));
-	});
-	
-	// for media that do not have a parent
-	$("[permlink="+permlink+"][data-src]").each(function( index ) {
-		$(this).replaceWith($(this)[0].outerHTML.replace("data-src=","src="));
-	});
+    $("[permlink="+permlink+"] [data-src]").each(function( index ) {
+        $(this).replaceWith($(this)[0].outerHTML.replace("data-src=","src="));
+    });
+    $("[permlink="+permlink+"] [data-srcset]").each(function( index ) {
+        $(this).replaceWith($(this)[0].outerHTML.replace("data-srcset=",
+                                                         "srcset="));
+    });
+
+    // for media that do not have a parent
+    $("[permlink="+permlink+"][data-src]").each(function( index ) {
+        $(this).replaceWith($(this)[0].outerHTML.replace("data-src=","src="));
+    });
 }
 
 
 // simulate link press when arrow keys are pressed
-document.onkeyup = KeyCheck;       
+document.onkeyup = KeyCheck;
 function KeyCheck(e) {
-  var KeyID = (window.event) ? event.keyCode : e.keyCode;
-  switch(KeyID) {
+    var KeyID = (window.event) ? event.keyCode : e.keyCode;
+    switch(KeyID) {
     case 37:
         $(".tkgal_link.prev")[0].click();
-    break;
+        break;
     case 39:
-         $(".tkgal_link.next")[0].click();
-    break;
-   }
-}
-
-// 
-$(document).ready(function() {
-	
-	$(".tkgal_link").click(function(e) {
-	  e.preventDefault();
-	  changeCurrent( $(this).attr("link") );
-	});
-  
-  $(".tkgal_container > *").swipe( {
-    swipeLeft:swipehandler,
-    swipeRight:swipehandler,
-    allowPageScroll:"auto" ,
-  });
-    
-  function swipehandler(event, direction) {
-    switch(direction) {
-      case "left":
         $(".tkgal_link.next")[0].click();
         break;
-      case "right":
-        $(".tkgal_link.prev")[0].click();
-        break;
-      default:
     }
-  };
+}
 
+$(document).ready(function() {
+    $(".tkgal_link").click(function(e) {
+        e.preventDefault();
+        changeCurrent( $(this).attr("link") );
+    });
+
+    $(".tkgal_container > *").swipe( {
+        swipeLeft:swipehandler,
+        swipeRight:swipehandler,
+        allowPageScroll:"auto"
+    });
+
+    function swipehandler(event, direction) {
+        switch(direction) {
+        case "left":
+            $(".tkgal_link.next")[0].click();
+            break;
+        case "right":
+            $(".tkgal_link.prev")[0].click();
+            break;
+        default:
+        }
+    }
 });
