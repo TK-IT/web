TK-IT/web

commit 074f4caa6444cd31c0f4caa5e4a535a9f210eb41

Use None date when we don't know
This break pagination on the image.html page

tkweb/apps/gallery/models.py
@@ -27,7 +27,8 @@ def file_name(instance, path):
     return '/'.join([gfyear, album_slug, newFilename])
 
 
-def get_exif_date_or_1970(filename):
+def get_exif_date(filename):
+    logger.debug('get_exif_date: called with filename: %s' % filename.name)
     try:
         image = PilImage.open(filename)
         info = image._getexif()
@@ -46,19 +47,27 @@ def get_exif_date_or_1970(filename):
                     s = exif['DateTime' + t]
                     if type(s) is tuple:
                         s = str(s[0])
+                    logger.debug('get_exif_date: found EXIF field DateTime%s. Parsed it as %s', t, s)
+
                     if 'SubsecTime' + t in exif:
                         ms = exif['SubsecTime' + t]
                         if type(ms) is tuple:
                             ms = str(ms[0])
+                        logger.debug('get_exif_date: found EXIF field SubsecTime. Parsed it as %s', ms)
                     else:
                         ms = '0'
-                    s += "." + ms
-                    return datetime.strptime(s, '%Y:%m:%d %H:%M:%S.%f')
-    except Exception as e:
-        logger.warning('Exception occurred in the slightly volatile get_exif_date_or_1970. Returning 1970:', e)
 
-    logger.info('get_exif_date_or_1970 could not get exif date. Returning 1970')
-    return datetime.fromtimestamp(0, tz=get_current_timezone())
+                    s += "." + ms
+                    dt = datetime.strptime(s, '%Y:%m:%d %H:%M:%S.%f')
+                    dt = dt.replace(tzinfo=get_current_timezone())
+
+                    logger.info('get_exif_date: returning exif date: %s', dt)
+                    return dt
+    except Exception as e:
+        logger.warning('get_exif_date: An exception occurred in this slightly volatile function.', exc_info=True)
+
+    logger.info('get_exif_date: could not get exif date. Returning none')
+    return None
 
 class Album(models.Model):
     class Meta:
@@ -108,7 +117,7 @@ class Image(models.Model):
 
     def clean(self):
         self.image.open('rb')
-        self.date = get_exif_date_or_1970(self.image)
+        self.date = get_exif_date(self.image)
 
         self.image.open('rb')  # open() does a seek(0)
         m = hashlib.sha1()

commit 03eb327d79cf4359978947be7c6d39203f3786f6

Use Paginator for image view.
The order of the view is now defined by the `order` in the model not
by the date.

This fixes the normal use of the prev/next links which is broken since
074f4caa6444cd31c0f4caa5e4a535a9f210eb41, however not js pagination from
tkgal.js.

tkweb/apps/gallery/models.py
@@ -81,15 +81,6 @@ class Album(models.Model):
     slug = models.SlugField()
     description = models.TextField(blank=True)
 
-    def number_of_images(self):
-        return self.images.count()
-
-    def prev(self, image_slug):
-        image = self.images.get(slug=image_slug)
-        prev = self.images.filter(date__lt=image.date).reverse[0]
-        print(prev)
-        return prev
-
     def __str__(self):
         return '%s: %s' % (self.gfyear, self.title)
 

tkweb/apps/gallery/views.py
@@ -4,6 +4,7 @@ from __future__ import absolute_import, unicode_literals, division
 from django.contrib.auth.decorators import permission_required
 from django.contrib.contenttypes.models import ContentType
 from django.core.exceptions import ValidationError
+from django.core.paginator import Paginator, EmptyPage
 from django.db.models import Max
 from django.shortcuts import get_object_or_404, get_list_or_404
 from django.shortcuts import render, redirect
@@ -35,23 +36,32 @@ def album(request, gfyear, album_slug):
 
 def image(request, gfyear, album_slug, image_slug, **kwargs):
     album = get_object_or_404(Album, gfyear=gfyear, slug=album_slug)
-    image = get_object_or_404(Image, slug=image_slug)
+    images = list(album.images.all()) # list() will force evaluation of the
+                                      # QuerySet. We can now use .index()
+    paginator = Paginator(images, 1)
+    start_image = get_object_or_404(Image, slug=image_slug)
+
+    try:
+        prev_image = paginator.page((images.index(start_image))+1-1)[0]
+        # +1 because paginator is 1-indexed, -1 to get the previous image
+    except EmptyPage:
+        # We are at the first image
+        prev_image = paginator.page(paginator.num_pages)[0]
+
+    try:
+        next_image = paginator.page((images.index(start_image))+1+1)[0]
+        # +1 because paginator is 1-indexed, +1 to get the next image
+    except EmptyPage:
+        # We are at the last image
+        next_image = paginator.page(1)[0]
+
+
     context = {
         'album': album,
-        'start_image': image,
+        'start_image': start_image,
+        'prev_image': prev_image,
+        'next_image': next_image,
     }
-    next_image = album.images.filter(date__gt=image.date)
-
-    try:
-        context['next_image'] = next_image[0]
-    except:
-        context['next_image'] = image
-        prev_image = album.images.filter(date__lt=image.date).reverse()
-    try:
-        context['prev_image'] = prev_image[0]
-    except:
-        context['prev_image'] = image
-
     return render(request, 'image.html', context)
 
 