35a13eefbc95226d744fa3655b941e912c4cbdb4
Mathias Dannesbo <neic@neic.dk>
2016-03-24 06:54:13 -0500

    Make slug form readonly

--- tkweb/apps/gallery/admin.py
+++ tkweb/apps/gallery/admin.py
@@ -2,7 +2,7 @@ from django import forms
 from django.contrib import admin
 from django.contrib.admin import TabularInline
 from django.core.exceptions import ValidationError
-from django.db.models import Max
+from django.db import models
 from sorl.thumbnail.admin import AdminImageMixin
 from tkweb.apps.gallery.models import Album, Image
 
@@ -10,6 +10,10 @@ from tkweb.apps.gallery.models import Album, Image
 class InlineImageAdmin(AdminImageMixin, TabularInline):
     model = Image
     extra = 0
+    formfield_overrides = { models.SlugField:
+                            { 'widget':
+                              forms.TextInput(attrs = { 'readOnly': 'True' })}}
+
 
     def has_add_permission(self, request):
         return False
@@ -32,5 +36,8 @@ class AlbumAdmin(admin.ModelAdmin):
     inlines = [InlineImageAdmin]
     form = AlbumAdminForm
     prepopulated_fields = { 'slug': ('title',), }
+    formfield_overrides = { models.SlugField:
+                            { 'widget':
+                              forms.TextInput(attrs = { 'readOnly': 'True' })}}
 
 admin.site.register(Album, AlbumAdmin)
