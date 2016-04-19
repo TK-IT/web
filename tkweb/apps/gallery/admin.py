d8f4b9a3c6c2376415197cca6a7bd10d2621dca2
Mathias Dannesbo <neic@neic.dk>
2016-04-19 13:55:40 +0200

    Show BaseMedia as TabularInline
    Introduces #43

--- tkweb/apps/gallery/admin.py
+++ tkweb/apps/gallery/admin.py
@@ -1,13 +1,12 @@
 from django import forms
 from django.contrib import admin
-from django.contrib.admin import TabularInline
 from django.core.exceptions import ValidationError
 from django.db import models
-from tkweb.apps.gallery.models import Album, Image
+from tkweb.apps.gallery.models import Album, BaseMedia
 
 
-class InlineImageAdmin(TabularInline):
-    model = Image
+class InlineBaseMediaAdmin(admin.TabularInline):
+    model = BaseMedia
     extra = 0
     fields = ( 'admin_thumbnail', 'date', 'caption', 'notPublic', 'slug', )
     readonly_fields = ( 'admin_thumbnail', 'slug', )
@@ -30,7 +29,7 @@ class AlbumAdminForm(forms.ModelForm):
 
 class AlbumAdmin(admin.ModelAdmin):
     list_display = ('title', 'gfyear', 'publish_date', 'slug')
-    inlines = [InlineImageAdmin]
+    inlines = [InlineBaseMediaAdmin]
     form = AlbumAdminForm
     prepopulated_fields = { 'slug': ('title',), }
     formfield_overrides = { models.SlugField:
