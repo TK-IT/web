9bfccdfd170ea16b9b0b0ccb7cc6760581147535
Mathias Dannesbo <neic@neic.dk>
2016-05-20 12:22:36 +0200

    Move the dev database one folder level up

--- tkweb/settings/dev.py
+++ tkweb/settings/dev.py
@@ -16,7 +16,7 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
     }
 }
 
