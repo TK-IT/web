b656ff705e5058af63a24e8bfbce8320a98cf883
Mathias Dannesbo <neic@neic.dk>
2016-06-01 19:18:32 +0200

    Move helper funtions from gallery.models to utils
    Also remved some unused imports

--- tkweb/apps/gallery/models.py
+++ tkweb/apps/gallery/models.py
@@ -1,9 +1,7 @@
 # encoding: utf8
 from __future__ import absolute_import, unicode_literals, division
 
-from PIL.ExifTags import TAGS
-from constance import config
-from datetime import date, datetime
+from datetime import date
 from django.contrib.contenttypes.fields import GenericForeignKey, GenericRelation
 from django.contrib.contenttypes.models import ContentType
 from django.core.validators import MinValueValidator, MaxValueValidator
@@ -11,84 +9,17 @@ from django.db import models
 from django.dispatch import receiver
 from django.utils.text import slugify
 from django.utils.encoding import python_2_unicode_compatible
-from django.utils.http import int_to_base36
-from django.utils.timezone import get_current_timezone
 from model_utils.managers import InheritanceManager
 from sorl.thumbnail import get_thumbnail
 from versatileimagefield.fields import VersatileImageField
 from versatileimagefield.image_warmer import VersatileImageFieldWarmer
-from PIL import Image as PilImage
-import hashlib
 import os
 import logging
+from tkweb.apps.gallery.utils import file_name, get_exif_date, get_gfyear
 
 FORCEDORDERMAX = 10000
 
 logger = logging.getLogger(__name__)
-
-def file_name(instance, path):
-    filename = os.path.basename(path)
-    sepFilename = os.path.splitext(filename)
-    newFilename = slugify(sepFilename[0]) + sepFilename[1]
-    gfyear = str(instance.album.gfyear)
-    album_slug = instance.album.slug
-
-    return '/'.join([gfyear, album_slug, newFilename])
-
-
-def get_exif_date(filename):
-    logger.debug('get_exif_date: called with filename: %s' % filename)
-    try:
-        image = PilImage.open(filename)
-        info = image._getexif()
-        if info is not None:
-            exif = {}
-            for tag, value in info.items():
-                decoded = TAGS.get(tag, tag)
-                exif[decoded] = value
-
-            for t in ["Original", "Digitized", ""]:
-                """
-                Check the exif fields DateTimeOriginal, DateTimeDigitized and
-                DateTime in that order. Return when the first is found.
-                """
-                if 'DateTime' + t in exif:
-                    s = exif['DateTime' + t]
-                    if type(s) is tuple:
-                        s = str(s[0])
-                    logger.debug('get_exif_date: found EXIF field DateTime%s. Parsed it as %s', t, s)
-
-                    if 'SubsecTime' + t in exif:
-                        ms = exif['SubsecTime' + t]
-                        if type(ms) is tuple:
-                            ms = str(ms[0])
-                        logger.debug('get_exif_date: found EXIF field SubsecTime. Parsed it as %s', ms)
-                    else:
-                        ms = '0'
-
-                    s += "." + ms
-
-                    if any(str(n) in s for n in range(1,10)):
-                        dt = datetime.strptime(s, '%Y:%m:%d %H:%M:%S.%f')
-                        dt = dt.replace(tzinfo=get_current_timezone())
-
-                        logger.info('get_exif_date: returning exif date: %s', dt)
-                        return dt
-
-                    logger.debug('get_exif_date: the DateTime%s field only contained zeros. Trying next field', t)
-
-    except AttributeError as e:
-        logger.info('get_exif_date: could not get exif data. This file is properly not a jpg or tif. Returning None')
-        return None
-
-    except Exception as e:
-        logger.warning('get_exif_date: An exception occurred in this slightly volatile function.', exc_info=True)
-
-    logger.info('get_exif_date: could not get exif date. Returning None')
-    return None
-
-def get_gfyear():
-    return config.GFYEAR
 
 @python_2_unicode_compatible
 class Album(models.Model):
