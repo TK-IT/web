76860feb2240d92a8a129769e4ed05013f6bb24c
Mathias Dannesbo <neic@neic.dk>
2016-06-08 17:21:02 +0200

    Better kerning with CM Typewriter
    Work on #48

--- tkweb/apps/tkbrand/templatetags/tkbrand.py
+++ tkweb/apps/tkbrand/templatetags/tkbrand.py
@@ -7,15 +7,17 @@ from tkweb.apps.tkbrand import util
 
 register = template.Library()
 
-html_tk = ('<span style="vertical-align: -0.038em">T</span>'
-           '<span style="font-weight: bold">&Aring;</span>'
-           'G'
-           '<span style="display: inline-block; transform: rotate(8deg);">E</span>'
-           '<span style="vertical-align: -0.057em;">K</span>'
-           '<span style="vertical-align: 0.020em; font-weight: bold;">A</span>'
-           '<span style="vertical-align: -0.057em; margin-left: 0.075em;">M</span>'
-           '<span style="display: inline-block; transform: rotate(-8deg); font-weight: bold; margin-left: 0.110em;">M</span>'
-           '<span style="margin-left: 0.075em;">ER</span>')
+html_tk = (
+    '<span style="vertical-align: -0.038em;">T</span>'
+    '<span style="font-weight: bold; margin-left: -0.05em;">Å</span>'
+    '<span style="margin-left: -0.05em;">G</span>'
+    '<span style="display: inline-block; transform: rotate(8deg);">E</span>'
+    '<span style="vertical-align: -0.057em; margin-left: 0.05em;">K</span>'
+    '<span style="vertical-align: 0.020em; font-weight: bold;">A</span>'
+    '<span style="vertical-align: -0.057em;">M</span>'
+    '<span style="display: inline-block; transform: rotate(-8deg); font-weight: bold; margin-left: 0.060em;">M</span>'
+    '<span style="margin-left: 0.05em;">E</span>'
+    '<span style="margin-left: 0.02em;">R</span>')
 
 html_tket = html_tk + '<span style="vertical-align: 0.057em">ET</span>'
 
