c415922182f183508803fb5ebb6c9a7663c869f2
Mathias Rav <FORM13@TAAGEKAMMERET.dk>
2016-06-23 18:28:55 +0200

    Remove useless groups

---
+++ idm/migrations/0019_remove_useless_groups.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+def remove_useless_groups(apps, schema_editor):
+    Group = apps.get_model('idm', 'Group')
+    Group.objects.exclude(type=0).delete()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('idm', '0018_rename_orgtitel_root'),
+    ]
+
+    operations = [
+        migrations.RunPython(remove_useless_groups),
+    ]
