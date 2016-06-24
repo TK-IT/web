4d46562f978d70a2041052ab73e8c65a81ca454e
Mathias Rav <FORM13@TAAGEKAMMERET.dk>
2016-06-24 13:29:57 +0200

    Populate Profile.gone

---
+++ idm/migrations/0031_populate_profile_gone.py
@@ -0,0 +1,24 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+def populate_profile_gone(apps, schema_editor):
+    Profile = apps.get_model('idm', 'Profile')
+    qs1 = Profile.objects.filter(gone_janej='ja')
+    qs2 = Profile.objects.filter(gone_janej='nej')
+    assert qs1.count() + qs2.count() == Profile.objects.all().count()
+    qs1.update(gone=True)
+    qs2.update(gone=False)
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('idm', '0030_profile_gone'),
+    ]
+
+    operations = [
+        migrations.RunPython(populate_profile_gone),
+    ]
