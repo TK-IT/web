46eb0b31dd07227fa58fca45c1c7f3563fa43f50
Mathias Rav <FORM13@TAAGEKAMMERET.dk>
2016-06-29 18:05:16 +0200

    Change MIDDLEWARE_CLASSES from a tuple to a list

--- tkweb/settings/base.py
+++ tkweb/settings/base.py
@@ -161,13 +161,13 @@ VERSATILEIMAGEFIELD_RENDITION_KEY_SETS = {
 # Middleware
 # ----------
 
-MIDDLEWARE_CLASSES = (
+MIDDLEWARE_CLASSES = [
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
-)
+]
 
 # Test
 # ----
