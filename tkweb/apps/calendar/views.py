614a6b5f7ee02161266ee71eacf241194d14517b
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2016-09-10 11:47:22 +0200

    Fix #84

--- tkweb/apps/calendar/views.py
+++ tkweb/apps/calendar/views.py
@@ -10,7 +10,7 @@ def kalender(request):
     yearAgo = date.today() - timedelta(days=365)
 
     futureEvents = Event.objects.filter(date__gt=(todayish))
-    pastEvents = Event.objects.filter(date__range=(yearAgo, todayish))
+    pastEvents = Event.objects.filter(date__range=(yearAgo, todayish)).reverse()
 
     context = {'futureEvents': futureEvents,
                'pastEvents': pastEvents}
