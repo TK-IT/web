6f137a89d4d2ded6c54ce230bb5d82e8465f4e3f
Mathias Rav <rav@cs.au.dk>
2016-10-08 15:37:41 +0200

    wip

--- regnskab/views.py
+++ regnskab/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import redirect, get_object_or_404
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, FormView
 from regnskab.forms import SheetCreateForm
 from regnskab.models import Sheet
 
 
 class SheetCreate(FormView):
     form_class = SheetCreateForm
+    template_name = 'regnskab/sheet_create.html'
 
     def form_valid(self, form):
         data = form.cleaned_data
@@ -17,5 +18,12 @@ class SheetCreate(FormView):
 
 
 class SheetDetail(TemplateView):
+    template_name = 'regnskab/sheet_detail.html'
+
     def get_sheet(self):
         return get_object_or_404(Sheet.objects, pk=self.kwargs['pk'])
+
+    def get_context_data(self, **kwargs):
+        context_data = super(SheetDetail, self).get_context_data(**kwargs)
+        context_data['sheet'] = self.get_sheet()
+        return context_data
