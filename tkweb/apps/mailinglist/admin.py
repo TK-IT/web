213d841c6f7d186fc1508648c3c7e52321cd8621
Mathias Rav <FORM13@TAAGEKAMMERET.dk>
2016-10-20 21:48:37 -0400

    Add links to SharedFile views in admin

--- tkweb/apps/mailinglist/admin.py
+++ tkweb/apps/mailinglist/admin.py
@@ -42,4 +42,45 @@ class Email(object):
     _meta = Meta()
 
 
+class SharedFileAdmin(admin.ModelAdmin):
+    def get_urls(self):
+        info = self.model._meta.app_label, self.model._meta.module_name
+        return [
+            url(r'^$',
+                RedirectView.as_view(url='/email/file/', permanent=False),
+                name='%s_%s_changelist' % info),
+            url(r'^add/$',
+                RedirectView.as_view(url='/email/file/upload/', permanent=False),
+                name='%s_%s_add' % info),
+        ]
+
+    def has_add_permission(self, request, obj=None):
+        return request.user.has_perm('mailinglist.send')
+
+    def has_delete_permission(self, *args, **kwargs):
+        return False
+
+    def has_change_permission(self, request, obj=None):
+        return request.user.has_perm('mailinglist.send')
+
+
+class SharedFile(object):
+    class Meta:
+        app_label = 'mailinglist'
+        object_name = 'SharedFile'
+        model_name = module_name = 'sharedfile'
+        verbose_name = verbose_name_plural = 'Vedhæftet fil'
+        abstract = False
+        swapped = False
+
+        def get_ordered_objects(self):
+            return False
+
+        def get_change_permission(self):
+            return 'change_%s' % self.model_name
+
+    _meta = Meta()
+
+
 admin.site.register([Email], MailinglistAdmin)
+admin.site.register([SharedFile], SharedFileAdmin)
