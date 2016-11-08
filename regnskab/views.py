df0c0af3be367530db1d20fe7fe0b1338dc76500
Mathias Rav <rav@cs.au.dk>
2016-11-08 21:03:12 +0100

    Output TK_PROFILES

--- regnskab/views.py
+++ regnskab/views.py
@@ -1,7 +1,9 @@
+import json
 from django.shortcuts import redirect, get_object_or_404
 from django.views.generic import TemplateView, FormView
 from regnskab.forms import SheetCreateForm
-from regnskab.models import Sheet, SheetRow
+from regnskab.models import Sheet, SheetRow, SheetStatus, parse_bestfu_alias, Profile, Alias
+from regnskab import config
 
 
 class SheetCreate(FormView):
@@ -51,4 +53,40 @@ class SheetUpdate(TemplateView):
     def get_context_data(self, **kwargs):
         context_data = super(SheetUpdate, self).get_context_data(**kwargs)
         context_data['sheet'] = self.get_sheet()
+
+        current_qs = SheetStatus.objects.filter(end_time=None)
+        current = set(current_qs.values_list('profile_id', flat=True))
+        profiles_qs = Profile.objects.all()
+        profiles_qs = profiles_qs.prefetch_related('title_set')
+        TITLE_ORDER = dict(BEST=0, FU=1, EFU=2)
+
+        alias_qs = Alias.objects.filter(end_time=None)
+        aliases = {}
+        for a in alias_qs:
+            aliases.setdefault(a.profile_id, []).append(a)
+
+        profiles = []
+        for p in profiles_qs:
+            t = []
+            in_current = 0 if p.id in current else 1
+            k = (in_current, 3, p.name)
+            for title in p.title_set.all():
+                t_k = (in_current, TITLE_ORDER[title.kind], -title.period, title.root)
+                k = min(k, t_k)
+                t.append(title.input_title())
+            for title in aliases.get(p.id, ()):
+                try:
+                    kind, root, period = parse_bestfu_alias(title.root, config.GFYEAR)
+                except ValueError:
+                    pass
+                else:
+                    t_k = (in_current, TITLE_ORDER[kind], -period, root)
+                    k = min(k, t_k)
+                t.append(title.root)
+            profiles.append(dict(titles=t, sort_key=k, name=p.name, id=p.pk))
+        profiles.sort(key=lambda x: x['sort_key'])
+        for i, x in enumerate(profiles):
+            x['sort_key'] = i
+        context_data['profiles_json'] = json.dumps(profiles, indent=2)
+
         return context_data
