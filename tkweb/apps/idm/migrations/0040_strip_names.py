28f744dcc202be494f4dc68e7a3d848dcee447b6
Mathias Rav <FORM13@TAAGEKAMMERET.dk>
2016-11-15 08:57:49 +0100

    Strip names of leading/trailing spaces

---
+++ tkweb/apps/idm/migrations/0040_strip_names.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+def strip_names(apps, schema_editor):
+    Profile = apps.get_model('idm', 'Profile')
+    for p in Profile.objects.all():
+        if p.name != p.name.strip():
+            p.name = p.name.strip()
+            p.save()
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('idm', '0039_gone_default'),
+    ]
+
+    operations = [
+        migrations.RunPython(strip_names),
+    ]
