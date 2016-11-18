6386754f8bf5a35e943a783eba2b01a5dfc25f4d
Mathias Rav <rav@cs.au.dk>
2016-11-18 11:08:54 +0100

    Fix convertalias

--- regnskab/management/commands/convertalias.py
+++ regnskab/management/commands/convertalias.py
@@ -14,8 +14,15 @@ class Command(BaseCommand):
         for a in qs:
             if is_title(a.root):
                 self.stdout.write(a.display_title(GFYEAR))
+                if a.root.startswith('FU'):
+                    kind = Title.FU
+                elif a.root.startswith('EFU'):
+                    kind = Title.EFU
+                else:
+                    kind = Title.BEST
                 new.append(Title(profile_id=a.profile_id,
                                  root=a.root,
+                                 kind=kind,
                                  period=a.period))
                 delete_ids.append(a.id)
         Title.objects.bulk_create(new)
