af02a3f327c808a45f47517c5323ff863c41ab5a
Mathias Rav <rav@cs.au.dk>
2016-11-30 18:13:23 +0100

    Add Transaction model

---
+++ regnskab/migrations/0009_transaction.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+from django.conf import settings
+import django.db.models.deletion
+
+
+profile_model = settings.TKWEB_IDM_MODULE.split('.')[-1] + '.Profile'
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        migrations.swappable_dependency(profile_model),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('regnskab', '0008_unit_price'),
+    ]
+
+    operations = [
+        migrations.CreateModel(
+            name='Transaction',
+            fields=[
+                ('id', models.AutoField(primary_key=True, verbose_name='ID', serialize=False, auto_created=True)),
+                ('kind', models.CharField(max_length=10, choices=[('payment', 'Betaling'), ('purchase', 'Diverse køb')])),
+                ('time', models.DateTimeField()),
+                ('amount', models.DecimalField(max_digits=9, decimal_places=2)),
+                ('note', models.CharField(blank=True, max_length=255)),
+                ('created_time', models.DateTimeField(auto_now_add=True)),
+                ('created_by', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
+                ('profile', models.ForeignKey(to=profile_model)),
+                ('session', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to='regnskab.Session')),
+            ],
+        ),
+    ]
--- regnskab/models.py
+++ regnskab/models.py
@@ -114,6 +114,28 @@ class Payment(models.Model):
         return '%.2f kr.' % self.amount
 
 
+class Transaction(models.Model):
+    PAYMENT, PURCHASE = 'payment', 'purchase'
+    KIND = [
+        (PAYMENT, 'Betaling'),
+        (PURCHASE, 'Diverse køb'),
+    ]
+
+    session = models.ForeignKey('Session', on_delete=models.SET_NULL,
+                                null=True, blank=False)
+    kind = models.CharField(max_length=10, choices=KIND)
+    profile = models.ForeignKey(Profile)
+    time = models.DateTimeField()
+    amount = models.DecimalField(max_digits=9, decimal_places=2)
+    note = models.CharField(max_length=255, blank=True)
+    created_by = models.ForeignKey(User, on_delete=models.SET_NULL,
+                                   null=True, blank=False)
+    created_time = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return '%.2f kr.' % self.amount
+
+
 def get_primary_titles(title_qs=None, period=None):
     if title_qs is None:
         title_qs = Title.objects.all()
