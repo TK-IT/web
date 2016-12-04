4525730d5271882d5c98d59833226db1719cce4b
Mathias Rav <rav@cs.au.dk>
2016-12-04 22:22:23 +0100

    Turns views into a module

---
+++ regnskab/views/__init__.py
@@ -0,0 +1,8 @@
+from .base import (
+    Home, SessionCreate, SheetCreate, SheetDetail, SheetRowUpdate,
+    EmailTemplateList, EmailTemplateUpdate, EmailTemplateCreate, SessionList,
+    SessionUpdate, EmailList, EmailDetail, EmailSend,
+    get_profiles_title_status, ProfileList, ProfileDetail,
+    TransactionBatchCreateBase, PaymentBatchCreate, PurchaseNoteList,
+    PurchaseBatchCreate, PaymentPurchaseList, BalancePrint,
+)
