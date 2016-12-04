98aeef4b5edcb4f42b27a5731619a07256c1f85d
Mathias Rav <rav@cs.au.dk>
2016-12-04 11:53:13 +0100

    Customize Alias admin form

--- regnskab/admin.py
+++ regnskab/admin.py
@@ -5,6 +5,21 @@ from regnskab.models import Alias, Transaction, Sheet, EmailTemplate, Session
 class AliasAdmin(admin.ModelAdmin):
     list_display = ('__str__', 'profile', 'period', 'start_time', 'end_time')
 
+    def get_fields(self, request, obj=None):
+        if obj and obj.pk:
+            # Modifying an existing
+            return ('profile', 'root', 'start_time', 'end_time',
+                    'created_by', 'period')
+        else:
+            # Creating a new
+            return ('profile', 'root')
+
+    def save_model(self, request, obj, form, change):
+        if not change:
+            obj.created_by = request.user
+            obj.start_time = timezone.now()
+        obj.save()
+
 
 class TransactionAdmin(admin.ModelAdmin):
     list_display = ('profile', 'kind', 'time', 'amount', 'note')
