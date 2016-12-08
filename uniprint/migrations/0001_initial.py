8d663efade923c45e0cba8e2587c5a3f453d1679
Mathias Rav <rav@cs.au.dk>
2016-12-08 16:51:04 +0100

    WIP

--- uniprint/migrations/0001_initial.py
+++ uniprint/migrations/0001_initial.py
@@ -16,10 +16,11 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Document',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('file', models.FileField(upload_to='')),
-                ('text', models.TextField(blank=True, null=True)),
+                ('text', models.TextField(null=True, blank=True)),
                 ('pages', models.IntegerField()),
+                ('pdfinfo', models.TextField(null=True, blank=True)),
                 ('created_time', models.DateTimeField(auto_now_add=True)),
                 ('created_by', models.ForeignKey(null=True, on_delete=django.db.models.deletion.SET_NULL, to=settings.AUTH_USER_MODEL)),
             ],
@@ -30,7 +31,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Printer',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('name', models.CharField(max_length=100)),
                 ('destination', models.CharField(max_length=100)),
             ],
@@ -41,7 +42,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Printout',
             fields=[
-                ('id', models.AutoField(auto_created=True, verbose_name='ID', primary_key=True, serialize=False)),
+                ('id', models.AutoField(primary_key=True, serialize=False, verbose_name='ID', auto_created=True)),
                 ('created_time', models.DateTimeField(auto_now_add=True)),
                 ('copies', models.PositiveIntegerField(default=1)),
                 ('duplex', models.BooleanField(default=True)),
