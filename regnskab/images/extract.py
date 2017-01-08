55a8874557b71e6c19ef7b3fed1df741c2173956
Mathias Rav <rav@cs.au.dk>
2017-01-08 10:55:47 +0100

    Detect skipped rows

--- regnskab/images/extract.py
+++ regnskab/images/extract.py
@@ -132,6 +132,18 @@ def extract_rows(sheet_image, input_grey,
         -np.minimum(row_avg, cutoff), [width],
         max_distances=[max_distance]))
     sheet_image.rows = [0] + (row_peaks / height).tolist() + [1]
+    row_heights = np.diff(sheet_image.rows)
+    m = np.median(row_heights)
+    # We expect every row height to be m.
+    # If a row height is more than 1.5 m, we skipped a row.
+    skipped = np.round((row_heights - m) / m)
+    fixed = []
+    for y, extra in zip(sheet_image.rows[:-1], skipped):
+        fixed.append(y)
+        for i in range(int(extra)):
+            fixed.append(y + (i+1) * m)
+    fixed.append(1)
+    sheet_image.rows = fixed
 
 
 def extract_rows_cols(sheet_image):
