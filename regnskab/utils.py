2c6bd198b910d1d93983968383d5e8659923536e
Mathias Rav <rav@cs.au.dk>
2017-03-08 18:01:23 -0500

    Add sum_vector/sum_matrix docstrings

--- regnskab/utils.py
+++ regnskab/utils.py
@@ -2,6 +2,16 @@ from django.db.models import F, Sum
 
 
 def sum_vector(qs, index_spec, value_spec):
+    '''
+    Sum queryset values, grouping over given dimension.
+
+    sum_matrix(Foo.objects.all(), 'c', 'z') turns into the SQL query:
+
+    SELECT Foo.c, SUM(Foo.z) FROM Foo GROUP BY Foo.c
+
+    The result is a mapping `d` such that d[x]
+    is the sum of z values where c = x.
+    '''
     qs = qs.order_by()
     qs = qs.annotate(index_spec=F(index_spec))
     qs = qs.values('index_spec')
@@ -15,6 +25,16 @@ def sum_vector(qs, index_spec, value_spec):
 
 
 def sum_matrix(qs, column_spec, row_spec, value_spec):
+    '''
+    Sum queryset values, grouping over two dimensions.
+
+    sum_matrix(Foo.objects.all(), 'c1', 'c2', 'z') turns into the SQL query:
+
+    SELECT Foo.c1, Foo.c2, SUM(Foo.z) FROM Foo GROUP BY Foo.c1, Foo.c2
+
+    The result is a nested mapping `d` such that d[x1][x2]
+    is the sum of z values where c1 = x1 and c2 = x2.
+    '''
     qs = qs.order_by()
     qs = qs.annotate(row_spec=F(row_spec), column_spec=F(column_spec))
     qs = qs.values('row_spec', 'column_spec')
