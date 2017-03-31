2e119382703623419fbd48113244b9fe4d709fab
Mathias Rav <FORM13@TAAGEKAMMERET.dk>
2017-03-31 14:43:37 -0400

    Implement set_visibility (#98)

--- tkweb/apps/gallery/views.py
+++ tkweb/apps/gallery/views.py
@@ -3,13 +3,16 @@ from __future__ import absolute_import, unicode_literals, division
 
 from django.contrib.auth.decorators import permission_required
 from django.core.exceptions import ValidationError
+from django.core.urlresolvers import reverse
 from django.db.models import Count
-from django.http import Http404
+from django.http import (
+    Http404, HttpResponse, HttpResponseBadRequest, HttpResponseRedirect)
 from django.shortcuts import get_object_or_404
 from django.shortcuts import render
 from django.views.decorators.http import require_POST
 from jfu.http import upload_receive, UploadResponse, JFUResponse
 from tkweb.apps.gallery.models import Album, BaseMedia, Image, GenericFile
+from tkweb.apps.gallery.forms import EditVisibilityForm
 import os
 
 
@@ -44,20 +47,39 @@ def album(request, gfyear, album_slug):
     files = album.basemedia.filter(visibility=BaseMedia.PUBLIC).select_subclasses()
     context = {'album': album,
                'files': files}
+
+    edit_visibility = request.user.has_perms('gallery.change_image')
+    file = album.basemedia.first()
+    if edit_visibility and file:
+        kwargs = dict(gfyear=album.gfyear, album_slug=album.slug,
+                      image_slug=file.slug)
+        context['edit_visibility_link'] = (
+            reverse('image', kwargs=kwargs) + '?v=1')
     return render(request, 'album.html', context)
 
 
 def image(request, gfyear, album_slug, image_slug, **kwargs):
     album = get_object_or_404(Album, gfyear=gfyear, slug=album_slug)
 
+    edit_visibility = (bool(request.GET.get('v')) and
+                       request.user.has_perms('gallery.change_image'))
+
+    qs = album.basemedia.all()
+    if not edit_visibility:
+        qs = qs.filter(visibility=BaseMedia.PUBLIC)
+    qs = qs.select_subclasses()
     # list() will force evaluation of the QuerySet. It is now iterable.
-    files = list(album.basemedia.exclude(notPublic=True).select_subclasses())
+    files = list(qs)
+    form = EditVisibilityForm(files)
     start_file = album.basemedia.filter(album=album, slug=image_slug).select_subclasses().first()
+    if edit_visibility:
+        for file, (pk, key) in zip(files, form.basemedias):
+            file.visibility_field = form[key]
 
     if not start_file:
         raise Http404("Billedet kan ikke findes")
 
-    if start_file.notPublic:
+    if start_file.notPublic and not edit_visibility:
         raise Http404("Billedet kan ikke findes")
 
     prev_files = files[1:]+files[:1]
@@ -70,6 +92,7 @@ def image(request, gfyear, album_slug, image_slug, **kwargs):
         'file_orders': file_orders,
         'start_file': start_file,
         'file_count': file_count,
+        'edit_visibility': edit_visibility,
     }
     return render(request, 'image.html', context)
 
@@ -132,3 +155,32 @@ def upload_delete(request, pk):
         success = False
 
     return JFUResponse(request, success)
+
+
+@require_POST
+@permission_required('gallery.change_image', raise_exception=True)
+def set_visibility(request):
+    try:
+        form = EditVisibilityForm.from_POST(request.POST)
+    except EditVisibilityForm.Missing as exn:
+        return HttpResponseBadRequest(str(exn))
+    if not form.is_valid():
+        return HttpResponseBadRequest(str(form.errors))
+    for pk, key in form.basemedias:
+        initial = form.fields[key].initial
+        value = form.cleaned_data[key]
+        if initial != value:
+            BaseMedia.objects.filter(pk=pk).update(visibility=value)
+
+    albums = list(Album.objects.filter(pk__in=form.album_pks))
+    for a in albums:
+        # Update isCoverFile
+        a.clean()
+
+    # Redirect to album
+    if albums:
+        album = albums[0]
+        kwargs = dict(gfyear=album.gfyear, album_slug=album.slug)
+        return HttpResponseRedirect(reverse('album', kwargs=kwargs))
+    else:
+        return HttpResponse('Synlighed på givne billeder er blevet opdateret')
