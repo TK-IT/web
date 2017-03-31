a221a5329fe4ff6a5c4511741fbad237118809ef
Mathias Rav <FORM13@TAAGEKAMMERET.dk>
2017-03-31 13:14:18 -0400

    Replace BaseMedia.notPublic with BaseMedia.visibility

---
+++ tkweb/apps/gallery/migrations/0011_remove_basemedia_notpublic.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('gallery', '0010_set_visibility'),
+    ]
+
+    operations = [
+        migrations.RemoveField(
+            model_name='basemedia',
+            name='notPublic',
+        ),
+    ]
--- tkweb/apps/gallery/models.py
+++ tkweb/apps/gallery/models.py
@@ -70,6 +70,20 @@ class BaseMedia(models.Model):
         (AUDIO, 'Audio'),
         (OTHER, 'Other'),
     )
+
+    PUBLIC = 'public'
+    DISCARDED = 'discarded'
+    SENSITIVE = 'sensitive'
+    DELETE = 'delete'
+    NEW = 'new'
+    VISIBILITY = (
+        (PUBLIC, 'Synligt'),
+        (DISCARDED, 'Frasorteret'),
+        (SENSITIVE, 'Skjult'),
+        (DELETE, 'Slet'),
+        (NEW, 'Ubesluttet'),
+    )
+
     type = models.CharField(max_length=1,
                                       choices=TYPE_CHOICES,
                                       default=OTHER)
@@ -78,7 +92,8 @@ class BaseMedia(models.Model):
     album = models.ForeignKey(Album, on_delete=models.CASCADE, related_name='basemedia')
 
     date = models.DateTimeField(null=True, blank=True, verbose_name='Dato')
-    notPublic = models.BooleanField(default=False, verbose_name='Skjult')
+    visibility = models.CharField(max_length=10, choices=VISIBILITY,
+                                  verbose_name='Synlighed', default=NEW)
     caption = models.CharField(
         max_length=200, blank=True, verbose_name='Overskrift')
 
@@ -97,6 +112,10 @@ class BaseMedia(models.Model):
             return self.image.admin_thumbnail()
 
     admin_thumbnail.short_description = 'Thumbnail'
+
+    @property
+    def notPublic(self):
+        return self.visibility != self.PUBLIC
 
     def __str__(self):
         return '%s' % (self.slug)
