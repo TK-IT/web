c5d54b4ef9fe325fe483649f90cd5f56de6e2397
Mathias Rav <FORM13@TAAGEKAMMERET.dk>
2017-04-22 14:35:27 -0400

    Add cronjob to delete images marked "delete"

--- cron.sh
+++ cron.sh
@@ -4,3 +4,4 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 source $DIR/venv/bin/activate
 
 python $DIR/manage.py updateical --settings=tkweb.settings.prod
+python $DIR/manage.py delete_marked_images --settings=tkweb.settings.prod
