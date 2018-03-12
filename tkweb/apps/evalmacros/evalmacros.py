72a79fd3198f62b9c338af8d1acb4844fb992ba7
Mathias Rav <FORM13@TAAGEKAMMERET.dk>
2018-03-12 21:23:34 +0100

    Revamp evalmacros

--- tkweb/apps/evalmacros/evalmacros.py
+++ tkweb/apps/evalmacros/evalmacros.py
@@ -1,95 +1,84 @@
 import markdown
 import random
 import re
+import shlex
 from django.template.loader import render_to_string
 from wiki.core.plugins import registry
 from wiki.core.plugins.base import BasePlugin
-from wiki.plugins.macros.mdx.macro import KWARG_RE
 import tkweb.apps.tkbrand.templatetags.tkbrand as tkbrand
 from constance import config
-from django.conf import settings as django_settings
 
-METHODS = ['hide_section', 'TK', 'TKAA', 'TKET', 'TKETAA', 'TKETs', 'TKETsAA',
-           'TKETS', 'TKETSAA', 'tk_prefix', 'tk_kprefix', 'tk_postfix',
-           'tk_prepostfix', 'tk_email']
+METHODS = [
+    'begin_hide', 'end_hide',
+    'TK', 'TKAA', 'TKET', 'TKETAA', 'TKETs', 'TKETsAA', 'TKETS', 'TKETSAA',
+    'tk_prefix', 'tk_kprefix', 'tk_postfix', 'tk_prepostfix', 'tk_email',
+]
 
 
 class EvalMacroExtension(markdown.Extension):
-
-    """ Macro plugin markdown extension for django-wiki. """
-
+    """Macro plugin markdown extension for django-wiki."""
     def extendMarkdown(self, md, md_globals):
         """ Insert MacroPreprocessor before ReferencePreprocessor. """
-        md.preprocessors.add('dw-macros', EvalMacroPreprocessor(md), '>html_block')
+        md.preprocessors.add(
+            'dw-macros', EvalMacroPreprocessor(md), '>html_block')
 
 
 class EvalMacroPreprocessor(markdown.preprocessors.Preprocessor):
-
-
     def run(self, lines):
+        pattern = (
+            # Start by matching one of the names in METHODS.
+            r"\[(?P<macro>%s)" % '|'.join(
+                re.escape(method_name) for method_name in METHODS) +
+            # Then match optional arguments of at most 300 characters.
+            r"(?:\s+(?P<args>[^]\n]{0,300}))?\]"
+        )
 
-        """
-        Modified from
-        https://github.com/django-wiki/django-wiki/blob/master/src/wiki/plugins/macros/mdx/macro.py
-        This also replaces inline macros.
-        """
+        def repl(mo):
+            macro = mo.group('macro')
+            try:
+                macro = macro.strip()
+                method = getattr(self, macro)
+                args_str = mo.group('args')
+                args = shlex.split(args_str) if args_str else ()
+                return method(*args)
+            except Exception as exn:
+                return '(%s.%s %r)' % (self.__class__.__name__, macro, exn)
 
-        _MACRO_RE = re.compile(
-            r"(\[(?P<macro>\w+)(?P<kwargs>\s'?\w+\:.+)*\])",
-            re.IGNORECASE)
+        return [re.sub(pattern, repl, line) for line in lines]
 
-        def _replace(m):
-            macro = m.group('macro').strip()
-            if macro in METHODS and hasattr(self, macro):
-                kwargs = m.group('kwargs')
-                if kwargs:
-                    kwargs_dict = {}
-                    for kwarg in KWARG_RE.finditer(kwargs):
-                        arg = kwarg.group('arg')
-                        value = kwarg.group('value')
-                        if value is None:
-                            value = True
-                        if isinstance(value, str):
-                            # If value is enclosed with ': Remove and
-                            # remove escape sequences
-                            if value.startswith("'") and len(value) > 2:
-                                value = value[1:-1]
-                                value = value.replace("\\\\", "¤KEEPME¤")
-                                value = value.replace("\\", "")
-                                value = value.replace("¤KEEPME¤", "\\")
-                        kwargs_dict[str(arg)] = value
-                    return getattr(self, macro)(**kwargs_dict)
-                else:
-                    return getattr(self, macro)()
-            else:
-                return m.string
+    def get_user_groups(self):
+        try:
+            return self._cached_user_groups
+        except AttributeError:
+            self._cached_user_groups = list(
+                self.markdown.user.groups.values_list('name', flat=True))
+            return self._cached_user_groups
 
-        return [_MACRO_RE.sub(_replace, line) for line in lines]
-
-
-    def hide_section(self, title='BEST', message=''):
+    def begin_hide(self, title='BEST'):
+        expanded = any(t.lower() == title.lower()
+                       for t in self.get_user_groups())
         html = render_to_string(
-            "evalmacros/hide_section.html",
+            "evalmacros/begin_hide.html",
             context={
                 'title': title,
-                'message': message,
                 'id': title+'-'+str(random.randrange(9999)),
-                'expanded': self.markdown.user.groups.filter(name__iexact=title).exists()
+                'expanded': expanded,
             })
         return self.markdown.htmlStash.store(html, safe=False)
 
-    hide_section.meta = {
+    def end_hide(self):
+        html = render_to_string("evalmacros/end_hide.html")
+        return self.markdown.htmlStash.store(html, safe=False)
+
+    begin_hide.meta = {
         'short_description': 'Skjul en sektion',
-        'help_text': ('Skjuler en sektion der kun er relevant for nogle '\
-                      'grupper eller personer. Andre kan stadig se sektionen '\
-                      'ved at trykke på en knap.'),
-        'example_code': ('[hide_section message:\'Noget der er '\
-                         'skjult for alle andre end BEST.]\n'
-                         '[hide_section title:\'KASS\' message:\'Noget der er '\
-                         'skjult for alle andre end KASS.]'),
-        'args': {'title': ('Personen eller gruppen indholdet ikke er skjult '\
-                           'for. Standard: BEST'),
-                 'message': 'Teksten der skal skjules.',
+        'help_text': ('Skjuler en sektion der kun er relevant for nogle ' +
+                      'grupper eller personer. Andre kan stadig se ' +
+                      'sektionen ved at trykke på en knap.'),
+        'example_code': ('[begin_hide KASS]\nNoget der er ' +
+                         'skjult for alle andre end KASS.\n[end_hide]'),
+        'args': {
+            'title': 'Personen eller gruppen indholdet ikke er skjult for.',
         },
     }
 
@@ -132,26 +121,37 @@ class EvalMacroPreprocessor(markdown.preprocessors.Preprocessor):
             '</table>'),
     }
 
-    def tk_prefix(self, title='', year=config.GFYEAR):
-        return tkbrand.tk_prefix((title, int(year)))
+    def _get_year(self, year):
+        if year in (None, ''):
+            return config.GFYEAR
+        year = int(year)
+        if year < 56:
+            year += 2000
+        elif year < 100:
+            year += 1900
+        return year
 
-    def tk_kprefix(self, title='', year=config.GFYEAR):
-        return tkbrand.tk_kprefix((title, int(year)))
+    def tk_prefix(self, year=None, title=''):
+        return tkbrand.tk_prefix((title, self._get_year(year)))
 
-    def tk_postfix(self, title='', year=config.GFYEAR):
-        return tkbrand.tk_postfix((title, int(year)))
+    def tk_kprefix(self, year=None, title=''):
+        return tkbrand.tk_kprefix((title, self._get_year(year)))
 
-    def tk_prepostfix(self, title, year=config.GFYEAR):
-        return tkbrand.tk_prepostfix((title, int(year)))
+    def tk_postfix(self, year=None, title=''):
+        return tkbrand.tk_postfix((title, self._get_year(year)))
 
-    def tk_email(self, title, year=config.GFYEAR):
-        return tkbrand.tk_email((title, int(year)))
+    def tk_prepostfix(self, title, year=None):
+        return tkbrand.tk_prepostfix((title, self._get_year(year)))
+
+    def tk_email(self, title, year=None):
+        return tkbrand.tk_email((title, self._get_year(year)))
+
 
 class EvalMacroPlugin(BasePlugin):
+    # TODO: settings.SLUG
+    slug = "evalmacros"
 
-    slug = "evalmacros" #settings.SLUG
+    markdown_extensions = [EvalMacroExtension()]
 
-    markdown_extensions = [
-        EvalMacroExtension()]
 
 registry.register(EvalMacroPlugin)
