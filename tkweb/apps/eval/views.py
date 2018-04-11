35ecf5c5084e516b02386c8ab0b92d2fa0e743d0
Mathias Dannesbo <neic@neic.dk>
2018-04-11 14:23:10 +0200

    WIP Add Simple list of all articles with timeouts

--- tkweb/apps/eval/views.py
+++ tkweb/apps/eval/views.py
@@ -1,3 +1,10 @@
+from tkweb.apps.eval.models import WikiArticleTimeout
 from django.shortcuts import render
 
-# Create your views here.
+
+def timeouts(request, **kwargs):
+    wats = WikiArticleTimeout.objects.all()
+    context = {'wats': wats,
+    }
+
+    return render(request, 'eval/timeouts.html', context)
