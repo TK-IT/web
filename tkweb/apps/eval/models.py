6d255d9d6e0871cec78572d76a272bbf8d4cc957
Mathias Dannesbo <neic@neic.dk>
2018-04-11 19:38:36 +0200

    WIP Add updated field to WikiArticleTimeout and populated it on save

--- tkweb/apps/eval/models.py
+++ tkweb/apps/eval/models.py
@@ -3,6 +3,7 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from tkweb.apps.evalmacros.evalmacros import MONTHS, parseTimeoutMonth
 from wiki.models import Article
+import datetime
 import re
 
 
@@ -10,7 +11,8 @@ class WikiArticleTimeout(models.Model):
     article = models.OneToOneField(
         Article,
         related_name="wikiArticleTimeout")
-    month = models.PositiveSmallIntegerField()
+    timeoutMonth = models.PositiveSmallIntegerField(null=True)
+    updated = models.DateField(null=True)
 
     def __str__(self):
         return '%s: %s' % (self.article, self.month)
@@ -20,18 +22,28 @@ class WikiArticleTimeout(models.Model):
 def wikiArticle_callback(sender, instance, **kwargs):
     lines = instance.current_revision.content
 
-    pattern = (r"\[timeout(?:\s+(?P<month>(%s)))\]" %
-               '|'.join(m for ml in MONTHS for m in ml))
-    mo = re.search(pattern, lines, re.IGNORECASE)
-    if mo is not None:
-        month = parseTimeoutMonth(mo.group('month'))
+    toPattern = (r"\[timeout(?:\s+(?P<month>(%s)))\]" %
+                 '|'.join(m for ml in MONTHS for m in ml))
+    toMo = re.search(toPattern, lines, re.IGNORECASE)
+    if toMo is not None:
+        timeout = parseTimeoutMonth(toMo.group('month'))
     else:
-        month = 0
+        timeout = None
+
+    upPattern = (r"\[updated(?:\s+(?P<title>[^\s\n]{0,300}))" +
+                 "(?:\s+(?P<date>\d{4}-\d{1,2}-\d{1,2}))\]")
+    upMo = re.search(upPattern, lines, re.IGNORECASE)
+    if upMo is not None:
+        updated = datetime.datetime.strptime(upMo.group('date'),
+                                             "%Y-%m-%d").date()
+    else:
+        updated = None
 
     try:
         wikiArticleTimeout = instance.wikiArticleTimeout
     except WikiArticleTimeout.DoesNotExist:
         wikiArticleTimeout = WikiArticleTimeout(article=instance)
 
-    wikiArticleTimeout.month = month
+    wikiArticleTimeout.timeoutMonth = timeout
+    wikiArticleTimeout.updated = updated
     wikiArticleTimeout.save()
