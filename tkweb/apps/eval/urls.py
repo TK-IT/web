59ff7e011fd0cc74d4e3dec5723baeeb0619c5cb
Mathias Dannesbo <neic@neic.dk>
2018-04-11 14:23:10 +0200

    WIP Add Simple list of all articles with timeouts

--- tkweb/apps/eval/urls.py
+++ tkweb/apps/eval/urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import url
 from django.views.generic.base import RedirectView
 from django_nyt.urls import get_pattern as get_nyt_pattern
 from wiki.urls import get_pattern as get_wiki_pattern
+import tkweb.apps.eval.views
 
 urlpatterns = [
     url(r'^$',
@@ -9,6 +10,12 @@ urlpatterns = [
                              permanent=False),
         name='evalindex'),
 
+    # django-wiki urls
     url(r'wiki/', get_wiki_pattern()),
     url(r'notifications/', get_nyt_pattern()),
+
+    # Other urls
+    url(r'^timeouts/$',
+        tkweb.apps.eval.views.timeouts,
+        name='eval_timeouts'),
     ]
