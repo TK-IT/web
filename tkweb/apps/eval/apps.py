f8f4c69deaae8a006c83d1507af0a04a2d28cd10
Mathias Dannesbo <neic@neic.dk>
2018-04-12 19:09:16 +0200

    Combine eval and evalmacros

--- tkweb/apps/eval/apps.py
+++ tkweb/apps/eval/apps.py
@@ -1,5 +1,10 @@
 from django.apps import AppConfig
+from django.utils.module_loading import autodiscover_modules
 
 
 class EvalConfig(AppConfig):
     name = 'eval'
+    verbose_name = "Evaluerings wiki"
+
+    def ready(self):
+        autodiscover_modules('evalmacros')
