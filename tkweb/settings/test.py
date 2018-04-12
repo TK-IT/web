d10e1947fc1bf20bde90af585cd78e944d7de94a
Mathias Dannesbo <neic@neic.dk>
2018-04-12 15:53:18 +0200

    Add test settings for without migrations

---
+++ tkweb/settings/test.py
@@ -0,0 +1,29 @@
+# encoding: utf8
+from tkweb.settings.base import *
+
+SECRET_KEY = 'This.is.not.a.secret.key'
+
+ALLOWED_HOSTS = ['localhost', '127.0.0.1']
+
+# This is not used
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, '../testdb.sqlite3'),
+    }
+}
+
+# TEST_RUNNER = 'rainbowtests.test.runner.RainbowDiscoverRunner' # Fancy colors
+
+RAINBOWTESTS_SHOW_MESSAGES = False
+
+
+class DisableMigrations(object):
+    def __contains__(self, item):
+        return True
+
+    def __getitem__(self, item):
+        return None
+
+
+MIGRATION_MODULES = DisableMigrations()
