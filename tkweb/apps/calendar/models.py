4fa9acb8a5e3c53fc239a8d4d7841353dde63d6e
Mathias Dannesbo <neic@neic.dk>
2018-04-16 15:36:10 +0200

    Add __str__ to caldendar Event

--- tkweb/apps/calendar/models.py
+++ tkweb/apps/calendar/models.py
@@ -13,6 +13,9 @@ class Event(models.Model):
     description = models.TextField()
     facebook = models.CharField(max_length=200)
 
+    def __str__(self):
+        return '%s: %s' % (self.date, self.title)
+
     def clean(self):
         """Extract facebook url from description"""
 
