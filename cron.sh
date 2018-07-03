4114e6f92fdf9bd67dfaf174f21747f546e5399d
Asger Hautop Drewsen <asgerdrewsen@gmail.com>
2018-07-03 13:23:37 +0200

    Fix use of pipenv in cron.sh

--- cron.sh
+++ cron.sh
@@ -4,4 +4,4 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd "$DIR" || exit
 
 pipenv run ./manage.py updateical --settings=tkweb.settings.prod
-python run ./manage.py delete_marked_images --settings=tkweb.settings.prod
+pipenv run ./manage.py delete_marked_images --settings=tkweb.settings.prod
