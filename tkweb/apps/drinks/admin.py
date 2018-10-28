7c43da5a44fa036cecc9e59bd594dea721c37231
Hogfeldt <hogfeldt38@gmail.com>
2018-10-28 12:34:03 +0100

    Created admin site for drink entity

--- tkweb/apps/drinks/admin.py
+++ tkweb/apps/drinks/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Drink, Sprut 
+
+class SprutInline(admin.TabularInline):
+    model = Sprut
+    extra = 1 
+
+class DrinkAdmin(admin.ModelAdmin):
+    fieldsets = [
+            (None, {'fields':
+                ['name','price']}),
+            ('Recipe', {'fields':
+                ['serving','soda']})]
+    inlines = [SprutInline]
+
+admin.site.register(Drink, DrinkAdmin)
