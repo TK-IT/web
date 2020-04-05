2999a7feb2b83c80f1361292502975f4c43764a2
Mathias Dannesbo <neic@neic.dk>
2020-04-05 15:03:13 +0200

    Add mariadb dev server

--- docker/docker-entrypoint.sh
+++ docker/docker-entrypoint.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 set -euo pipefail
 
+# TODO wait for database to be ready in a proper way
+sleep 20
+
+# We run migration on container startup both in development and production. We
+# do this because: 1, we never run more than one instance of the web server on
+# the same database, 2, we don't have problems with some minutes of downtime if
+# somethings goes wrong, 3, we always deploy by hand and 4, it simple. See
+# https://pythonspeed.com/articles/schema-migrations-server-startup/ why you
+# shouldn't do this if any of the above does not apply.
+./manage.py migrate
+
 ./manage.py collectstatic --noinput --clear
 
 exec "$@"
--- tkweb/settings/dev.py
+++ tkweb/settings/dev.py
@@ -12,9 +12,12 @@ ALLOWED_HOSTS = ['localhost', '127.0.0.1']
 # https://docs.djangoproject.com/en/1.7/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, '../db.sqlite3'),
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "HOST": "tkweb-db",
+        "NAME": "tkweb",
+        "USER": "tkweb",
+        "PASSWORD": "tkweb",
     }
 }
 
