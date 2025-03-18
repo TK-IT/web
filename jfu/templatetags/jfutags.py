972375963ad0a1dd81e67004517949b1f6f1afbe
Mathias Rav <FORM13@TAAGEKAMMERET.dk>
2025-03-18 20:35:42 +0100

    jfu: Updates for new Django

--- jfu/templatetags/jfutags.py
+++ jfu/templatetags/jfutags.py
@@ -1,16 +1,16 @@
-from django.core.context_processors import csrf
-from django.core.urlresolvers import reverse
-from django.template import Library, Context, loader
+from django.template.context_processors import csrf
+from django.urls import reverse
+from django.template import Library, loader
 
 register = Library()
 
 @register.simple_tag( takes_context = True )
 def jfu(
-        context,
-        template_name = 'jfu/upload_form.html',
-        upload_handler_name = 'jfu_upload',
-        *args, **kwargs
-    ):
+    context,
+    template_name = 'jfu/upload_form.html',
+    upload_handler_name = 'jfu_upload',
+    *args, **kwargs
+):
     """
     Displays a form for uploading files using jQuery File Upload.
 
@@ -21,19 +21,19 @@ def jfu(
     Any additionally supplied positional and keyword arguments are directly
     forwarded to the named custom upload-handling URL.
     """
-    context.update( { 
+    ctx = { 
         'JQ_OPEN'  : '{%',
         'JQ_CLOSE' : '%}',
         'upload_handler_url': reverse(
             upload_handler_name, args = args, kwargs = kwargs
         ),
-    } )
+    }
 
     # Use the request context variable, injected
     # by django.core.context_processors.request,
     # to generate the CSRF token.
-    context.update( csrf( context.get('request') ) )
+    ctx.update( csrf( context.get('request') ) )
 
     t = loader.get_template( template_name )
 
-    return t.render( Context( context ) )
+    return t.render(ctx)
